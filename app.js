fbda0caefdbd4c4f55d3c0a2d29fb3d8c17b63d1
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-07-02 15:48:42 +0300

    Handle post request

--- app.js
+++ app.js
@@ -1,21 +1,44 @@
-const e = require('express');
 const fs = require('fs');
+const express = require('express');
 
 const port = 3000;
-const app = e();
+const app = express();
 
-const tours = fs.readFileSync(
-  `${__dirname}/dev-data/data/tours-simple.json`,
-  'utf-8'
-);
+app.use(express.json());
+
+const toursJSONPath = `${__dirname}/dev-data/data/tours-simple.json`;
+
+let tours = JSON.parse(fs.readFileSync(toursJSONPath, 'utf-8'));
 
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'success',
-    result: tours.length(),
+    result: tours.length,
     data: {
       tours,
     },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+
+  fs.writeFile(toursJSONPath, JSON.stringify(tours), 'utf-8', (err) => {
+    if (err) {
+      res.status(500).json({
+        status: 'error',
+        message: 'Failed to write to tours file',
+      });
+    } else {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
   });
 });
 
