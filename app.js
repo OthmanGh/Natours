2b0425ed9a95613438322e866be42f99ac0196e8
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-07-02 16:14:51 +0300

    Add checkers for getting single tour in case it don't exist

--- app.js
+++ app.js
@@ -23,6 +23,14 @@ app.get('/api/v1/tours', (req, res) => {
 app.get('/api/v1/tours/:id', (req, res) => {
   const { id } = req.params;
   const tour = tours.find((tour) => tour.id === Number(id));
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid id',
+    });
+  }
+
   res.status(200).json({
     status: 'success',
     data: {
