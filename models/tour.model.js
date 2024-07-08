77b1e1f1ad2d92eaa9e2109fdba9debfcb370ca1
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-07-09 00:08:42 +0300

    Modelling Tours

--- models/tour.model.js
+++ models/tour.model.js
@@ -5,17 +5,70 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, 'A tour name is required'],
     unique: true,
+    trim: true,
+  },
+
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a max group size'],
+  },
+
+  difficulty: {
+    type: String,
+    require: [true, 'A tour must have a difficulty'],
+  },
+
+  ratingAverage: {
+    type: Number,
+    default: 4.5,
+  },
+
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
 
   price: {
     type: Number,
-    required: [true, 'A tour price is required'],
+    required: [true, 'A tour must have a price'],
+  },
+
+  priceDiscount: Number,
+
+  summary: {
+    type: String,
+    required: [true, 'A tour must have a summary'],
+    trim: true,
   },
 
   rate: {
     type: Number,
     default: 4.5,
   },
+
+  description: {
+    type: String,
+    trim: true,
+  },
+
+  imageCover: {
+    type: String,
+    require: [true, 'A tour must have an image cover'],
+  },
+
+  images: [String],
+
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+
+  startDates: [Date],
 });
 
 const Tour = new mongoose.model('Tour', tourSchema);
