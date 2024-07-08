4b8f11155709677eef084944974c3888dd0eea57
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-07-08 20:30:20 +0300

    Connect to mongodb database

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
-const dotenv = require('dotenv');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 dotenv.config({
-  path: './config.env',
+  path: "./config.env",
 });
 
-const app = require('./app');
-console.log(process.env);
+const DB = process.env.DB.replace("<PASSWORD>", process.env.DB_PASSWORD);
+mongoose.connect(DB).then(() => {
+  console.log("MONGODB Connected");
+});
+
+const app = require("./app");
 
 const port = process.env.PORT || 3000;
 
