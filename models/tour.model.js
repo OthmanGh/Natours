8798a06b2e1660d624438579017160ae35c1eab8
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-07-15 16:53:41 +0300

    Use build-in validators for tours schema

--- models/tour.model.js
+++ models/tour.model.js
@@ -8,6 +8,8 @@ const tourSchema = new mongoose.Schema(
       required: [true, 'A tour name is required'],
       unique: true,
       trim: true,
+      maxlength: [40, 'A tour name must have less or equal then 40 characters'],
+      minlength: [10, 'A tour name must have more or equal then 10 characters'],
     },
 
     slug: String,
@@ -25,11 +27,17 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       require: [true, 'A tour must have a difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either: easy, medium, difficult',
+      },
     },
 
     ratingAverage: {
       type: Number,
       default: 4.5,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0'],
     },
 
     ratingsQuantity: {
