e9f09c79809168873746dd8da6dda45e3d75d4a8
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-07-16 13:47:45 +0300

    <Error Handling> - Handle errors during developement & production

--- controllers/error.controller.js
+++ controllers/error.controller.js
@@ -1,11 +1,35 @@
+const sendErrorDev = (error, res) => {
+  res.status(error.statusCode).json({
+    status: error.status,
+    message: error.message,
+    stack: error.stack,
+    error,
+  });
+};
+
+const sendErrorProd = (error, res) => {
+  if (error.isOperational) {
+    res.status(error.statusCode).json({
+      status: error.status,
+      message: error.message,
+    });
+  } else {
+    console.error('ERROR 💥', error);
+
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong',
+    });
+  }
+};
+
 module.exports = (error, req, res, next) => {
   error.statusCode = error.statusCode || 500;
   error.status = error.status || 'error';
 
-  res.status(error.statusCode).json({
-    status: error.status,
-    message: error.message,
-  });
-
-  new appError(error.message);
+  if (process.env.NODE_ENV === 'developement') {
+    sendErrorDev(error, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(error, res);
+  }
 };
