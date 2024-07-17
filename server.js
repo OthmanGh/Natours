652a9684d7d276cc76ad00d284d942162c6c05d8
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-07-17 18:27:42 +0300

    <Error Handling> - Unhandled rejections

--- server.js
+++ server.js
@@ -4,15 +4,30 @@ dotenv.config({
   path: './config.env',
 });
 
-const DB = process.env.DB.replace('<PASSWORD>', process.env.DB_PASSWORD);
-mongoose.connect(DB).then(() => {
-  console.log('MONGODB Connected');
-});
-
 const app = require('./app');
+
+const DB = process.env.DB.replace('<PASSWORD>', process.env.DB_PASSWORD);
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log('MONGODB Connected');
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+console.log(`Environment: ${process.env.NODE_ENV}`);
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION: 💥 Shutting down');
+  server.close(() => {
+    process.exit(1);
+  });
 });
