20a686e1fc91577a8363635e0dc010ea83966985
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-07-17 22:51:23 +0300

    filtering only necessary data needed for creating new user

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,7 +2,12 @@ const catchAsync = require('../utils/catchAsync');
 const User = require('../models/user.model');
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
 
   res.status(201).json({
     status: 'success',
