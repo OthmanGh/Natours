adb6467ee9a334579f7561bd225f5e564cddd301
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-07-18 12:35:26 +0300

    Implement password checker function

--- models/user.model.js
+++ models/user.model.js
@@ -25,6 +25,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Please provide a password'],
     minlength: 8,
+    select: false,
   },
 
   passwordConfirm: {
@@ -46,6 +47,13 @@ userSchema.pre('save', async function (next) {
   this.passwordConfirm = undefined;
   next();
 });
+
+userSchema.methods.isPasswordCorrect = async function (
+  candidatePassword,
+  userPassword,
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
 
 const User = mongoose.model('User', userSchema);
 
