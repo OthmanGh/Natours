83545fc62d05b233a615ce2233b52d0855035621
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-07-18 12:35:54 +0300

    Add login route

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,6 +4,7 @@ const userController = require('../controllers/user.controller');
 const authController = require('../controllers/auth.controller');
 
 router.route('/signup').post(authController.signup);
+router.route('/login').post(authController.login);
 
 router
   .route('/')
