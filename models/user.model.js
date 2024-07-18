e7c877e658036de8aeb8d33b71222b486032daad
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-07-19 01:50:34 +0300

    Add role field to user model

--- models/user.model.js
+++ models/user.model.js
@@ -19,6 +19,12 @@ const userSchema = new mongoose.Schema({
     validate: [validator.isEmail, 'Please provide a valid email'],
   },
 
+  role: {
+    type: String,
+    enum: ['user', 'guide', 'lead-guide', 'admin'],
+    default: 'user',
+  },
+
   photo: String,
 
   password: {
